c5d267de0486e2e020cdc0809dfa3799fb9bc908
YTX <gy5010@qq.com>
2026-08-31 16:46:39 +0800

    Initial commit

---
+++ src/main/java/com/campus/team/TeamMatchingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.campus.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMatchingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMatchingSystemApplication.class, args);
+
+    }
+
+}
